2485a060381d294639d717a9c7bcc9bc318d4e1b
Alex Manarpies <alex.manarpies@dpgmedia.be>
2020-01-19 14:32:23 +0100

    Add column test + update README.md

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 CSS Flexbox implementation for UIKit.
 Work in progress.
+
+## Usage
+
+### `flexDirection` and `flexWrap`
+
+#### Wrap Rows
+
+```swift
+view.flexDirection = .row
+view.flexWrap = .wrap
+```
+
+![Row wrap example](https://github.com/jarroolabs/UIFlexView/raw/master/UIFlexViewTests/__Snapshots__/UIFlexViewTests/testRowWrap.8.png)
+
+#### Wrap Columns
+
+```swift
+view.flexDirection = .column
+view.flexWrap = .wrap
+```
+
+![Row wrap example](https://github.com/jarroolabs/UIFlexView/raw/master/UIFlexViewTests/__Snapshots__/UIFlexViewTests/testColumnWrap.8.png)
--- UIFlexViewTests/UIFlexViewTests.swift
+++ UIFlexViewTests/UIFlexViewTests.swift
@@ -13,8 +13,6 @@ import UIFlexView
 class UIFlexViewTests: XCTestCase {
 
     func testRowWrap() {
-        // record = true
-        
         let boxSize = CGSize(width: 100, height: 100)
         let viewSize = CGSize(width: 320, height: 320)
 
@@ -52,6 +50,48 @@ class UIFlexViewTests: XCTestCase {
         flexView.addSubview(Box.staticallySized(boxSize, label: "9"))
         assertSnapshot(matching: flexView, as: .image)
         
+        flexView.addSubview(Box.staticallySized(boxSize, label: "10"))
+        assertSnapshot(matching: flexView, as: .image)
+    }
+
+    func testColumnWrap() {
+        let boxSize = CGSize(width: 100, height: 100)
+        let viewSize = CGSize(width: 320, height: 320)
+
+        let flexView = UIFlexView()
+
+        flexView.frame.size = viewSize
+        flexView.backgroundColor = .lightGray
+        flexView.flexDirection = .column
+        flexView.flexWrap = .wrap
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "1"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "2"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "3"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "4"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "5"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "6"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "7"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "8"))
+        assertSnapshot(matching: flexView, as: .image)
+
+        flexView.addSubview(Box.staticallySized(boxSize, label: "9"))
+        assertSnapshot(matching: flexView, as: .image)
+
         flexView.addSubview(Box.staticallySized(boxSize, label: "10"))
         assertSnapshot(matching: flexView, as: .image)
     }
